32ca417943d7b080aefb85151ec97cd670639603
YashLT224 <yashv@lambdatest.com>
2021-10-08 12:48:12 +0530

    react native beginners projec

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <CarItem />
-      <StatusBar style="auto " />
+      <StatusBar style="auto" />
     </View>
   );
 }
